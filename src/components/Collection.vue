5627bd8493a6c52812d41db9fe86af9ee76281c8
Nuria Ciscar <nuriaciscar@MacBook-Pro-de-MacBook.local>
2021-11-29 23:39:00 +0100

    Added Collection component with many images

--- src/components/Collection.vue
+++ src/components/Collection.vue
@@ -1,116 +1,75 @@
 <template>
-  <div class="home">
-    <section class="home__image">
-      <img
-        class="home__image__big"
-        src="https://farm4.staticflickr.com/3935/15363200788_026cc5119e_b.jpg"
-        alt="Description of image"
-      />
-      <p class="home__image__date">INDIA 2 YEARS AGO</p>
-    </section>
-    <section class="home__collections">
-      <div class="home__collections__single">
+   <section class="collection">
+      <div class="collection__single">
         <img
-          class="home__collections__single__image"
+          class="collection__single__image"
           src="https://farm4.staticflickr.com/3935/15363200788_026cc5119e_b.jpg"
           alt="Description of image"
           width="100px"
           height="65px"
         />
         <img
-          class="home__collections__single__image"
+        class="collection__single__image"
           src="https://farm4.staticflickr.com/3935/15363200788_026cc5119e_b.jpg"
           alt="Description of image"
           width="69px"
           height="94px"
         />
-        <div class="home__collections__single__text">
-          <p class="home__collections__single__text__date">June - August 2021</p>
-          <p class="home__collections__single__text__name">Italia summer 2021</p>
+        <div class="collection__single__text">
+          <p class="collection__single__text__date">June - August 2021</p>
+          <p class="collection__single__text__name">Italia summer 2021</p>
         </div>
         <img
-          class="home__collections__single__image"
+           class="collection__single__image"
           src="https://farm4.staticflickr.com/3935/15363200788_026cc5119e_b.jpg"
           alt="Description of image"
           width="100px"
           height="65px"
         />
         <img
-          class="home__collections__single__image"
+           class="collection__single__image"
           src="https://farm4.staticflickr.com/3935/15363200788_026cc5119e_b.jpg"
           alt="Description of image"
           width="100px"
           height="65px"
         />
         <img
-          class="home__collections__single__image"
+           class="collection__single__image"
           src="https://farm4.staticflickr.com/3935/15363200788_026cc5119e_b.jpg"
           alt="Description of image"
           width="100px"
           height="65px"
         />
         <img
-          class="home__collections__single__image"
+           class="collection__single__image"
           src="https://farm4.staticflickr.com/3935/15363200788_026cc5119e_b.jpg"
           alt="Description of image"
           width="100px"
           height="65px"
         />
         <img
-          class="home__collections__single__image"
+           class="collection__single__image"
           src="https://farm4.staticflickr.com/3935/15363200788_026cc5119e_b.jpg"
           alt="Description of image"
           width="100px"
           height="65px"
         />
-      </div>
-    </section>
-  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: "Home",
-  components: {},
+  name: "Collection",
+  data: () => ({}),
 });
 </script>
 
-<style lang="scss">
-@import "../styles/variables";
-@import "../styles/mixins";
-.home {
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  width: 100%;
-  margin: 0;
-  box-sizing: border-box;
-  justify-content: space-between;
-  &__image {
-    width: 100%;
-    margin: 0;
-    padding: 0;
-    height: 280px;
-    position: fixed;
-    z-index: 3;
-    &__big {
-      width: 100%;
-      height: 210px;
-      margin: 0;
-      padding-top: 65px;
-      object-fit: cover;
-    }
-    &__date {
-      font-family: "Public Sans", sans-serif;
-      font-size: 12px;
-      padding-top: 6px;
-      padding-left: 23px;
-    }
-  }
+<style scoped lang="scss">
+@import "../styles/_mixins.scss";
+@import "../styles/_variables.scss";
 
-  &__collections {
+&__collection {
     width: 100vw;
     margin-top: 45px;
     height: 600px;
@@ -155,5 +114,5 @@ export default defineComponent({
       }
     }
   }
-}
+
 </style>

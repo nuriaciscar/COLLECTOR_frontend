efd2a1babe97a29f069618e434ad78b4c68a7ace
Nuria Ciscar <nuriaciscar@MacBook-Pro-de-MacBook.local>
2021-11-30 17:57:30 +0100

    Added fetchLoadCollection action to get a collection by id

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -7,6 +7,11 @@ const actions = {
     const { data } = await axios.get(`${process.env.VUE_APP_API_URL}/collections`);
     commit("loadCollections", data);
   },
+  async fetchLoadCollection({ commit }: ActionContext<State, State>, id: string): Promise<void> {
+    const { data } = await axios.get(`${process.env.VUE_APP_API_URL}/${id}`);
+    commit("loadCollection", data);
+  },
+
   async fetchLoadImages({ commit }: ActionContext<State, State>): Promise<void> {
     const { data } = await axios.get(`${process.env.VUE_APP_API_URL}/image`);
     commit("loadImages", data);

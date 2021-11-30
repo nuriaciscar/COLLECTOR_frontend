c3af0621a5f6513a2c4846f90431e62c84ad49b0
Nuria Ciscar <nuriaciscar@MacBook-Pro-de-MacBook.local>
2021-11-30 12:03:21 +0100

    Added fetchCollections action to get collections from api

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,3 +1,13 @@
-const actions = {};
+import axios from "axios";
+import { ActionContext } from "vuex";
+import { State } from "@/types/interfaces";
+
+const actions = {
+  async fetchCollections({ commit }: ActionContext<State, State>): Promise<void> {
+    const { data } = await axios.get(`${process.env.VUE_APP_API_URL}/collections`);
+
+    commit("loadCollections", data);
+  },
+};
 
 export default actions;

fce270858d330b76adc5442fe3e97aba8e02fd8b
Nuria Ciscar <nuriaciscar@MacBook-Pro-de-MacBook.local>
2021-11-30 19:27:45 +0100

    Fixed fetchLoginUser action adding localStorage to get user

--- src/store/mutations.ts
+++ src/store/mutations.ts
@@ -1,4 +1,4 @@
-import { State, Collection, Image } from "@/types/interfaces";
+import { State, Collection, Image, User } from "@/types/interfaces";
 
 const mutations = {
   loadCollections(state: State, payload: Array<Collection>): void {
@@ -10,9 +10,9 @@ const mutations = {
   loadImages(state: State, payload: Array<Image>): void {
     state.images = payload;
   },
-  // loginUser(state: State, payload): void {
-  //   state.user = payload;
-  // },
+  loginUser(state: State, payload: User): void {
+    state.user = payload;
+  },
 };
 
 export default mutations;

d7c35d3b1dfb59756de1e839cd5aa04144762da7
Nuria Ciscar <nuriaciscar@MacBook-Pro-de-MacBook.local>
2021-12-01 20:32:33 +0100

    Added DetailCollection path on routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import Home from "../views/Home.vue";
 import LoginPage from "../views/LoginPage.vue";
+import DetailCollection from "../views/DetailCollection.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -12,6 +13,11 @@ const routes: Array<RouteRecordRaw> = [
     path: "/login",
     name: "LoginPage",
     component: LoginPage,
+  },
+  {
+    path: "/collections/:id",
+    name: "DetailCollection",
+    component: DetailCollection,
   },
 ];
 

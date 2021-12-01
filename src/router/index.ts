c11a0fb398b391844d5bf229bd2e8c8ae720684f
Nuria Ciscar <nuriaciscar@MacBook-Pro-de-MacBook.local>
2021-12-01 22:46:40 +0100

    Added path to DetailImage on routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import Home from "../views/Home.vue";
 import LoginPage from "../views/LoginPage.vue";
 import DetailCollection from "../views/DetailCollection.vue";
+import DetailImage from "../views/DetailImage.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -18,6 +19,11 @@ const routes: Array<RouteRecordRaw> = [
     path: "/collections/:id",
     name: "DetailCollection",
     component: DetailCollection,
+  },
+  {
+    path: "/image/:id",
+    name: "DetailImage",
+    component: DetailImage,
   },
 ];
 
---
+++ src/views/DetailImage.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="detail">
+    <div class="detail__top">
+      <router-link to="/collections">
+        <img src="../assets/next.png" class="back" width="25" height="13" />
+      </router-link>
+    </div>
+    <img :src="image" :alt="image.description" width="65" height="35" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { mapActions, mapState } from "vuex";
+import { useRoute } from "vue-router";
+
+export default defineComponent({
+  name: "DetailImage",
+
+  computed: {
+    ...mapState(["image"]),
+  },
+  methods: {
+    ...mapActions(["fetchLoadImage"]),
+  },
+
+  mounted() {
+    const route = useRoute();
+    const { id } = route.params;
+    this.fetchLoadImage(id);
+  },
+});
+</script>
+
+<style scoped lang="scss">
+@import "../styles/_mixins.scss";
+@import "../styles/_variables.scss";
+.detail {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: space-around;
+  align-items: flex-start;
+
+  &__top {
+    display: flex;
+    flex-direction: row;
+    border: none;
+    justify-content: center;
+    align-items: center;
+    width: 340px;
+    margin-top: 200px;
+
+    &__title {
+      font-weight: inherit;
+      font-size: 50px;
+    }
+  }
+}
+</style>

a7243ffc1c0a89b11209ff1dec45b8052aba84f2
Nuria Ciscar <nuriaciscar@MacBook-Pro-de-MacBook.local>
2021-12-01 10:10:08 +0100

    Installed font awesome and added on main ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import { createApp } from "vue";
 
-createApp(App).use(store).use(router).mount('#app');
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import store from "./store";
+import router from "./router";
+import App from "./App.vue";
+
+library.add(fas);
+library.add(fab);
+library.add(far);
+
+dom.watch();
+
+createApp(App).use(store).use(router).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
